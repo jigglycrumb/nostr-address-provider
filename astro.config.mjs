34bac4065061d0c24046e1e16166ebe71acc50ca
hpcodecraft <thesquidpeople@gmail.com>
2023-02-20 13:32:53 +0100

    fix: form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig();
+export default defineConfig({
+  integrations: [react()],
+  output: "server",
+  adapter: vercel(),
+});
