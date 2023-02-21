7b5fec489e892c3d1b6dfc6358ee553ba75a4fd9
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2023-02-21 18:19:02 +0100

    feat: react port (#5)

---
+++ src/components/UserCount.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { loadUsers } from "../utils";
+
+type UserCountProps = {
+  host: string;
+};
+
+const updateInterval = 30_000;
+
+export const UserCount = ({ host }: UserCountProps) => {
+  const [userCount, setUserCount] = useState<boolean | number>(false);
+
+  const updateUserCount = (users: Record<string, string>) => {
+    console.info("updating user count");
+    const userCount = Object.keys(users).length;
+    setUserCount(userCount);
+  };
+
+  useEffect(() => {
+    loadUsers(host).then(updateUserCount);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      loadUsers(host).then(updateUserCount);
+    }, updateInterval);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <section className="box user-count">
+      {!userCount ? (
+        <strong>Loading…</strong>
+      ) : (
+        <div>
+          <strong>Total users verified: {userCount}</strong> 🎉
+        </div>
+      )}
+    </section>
+  );
+};
---
+++ src/components/Verification/VerificationForm.tsx
@@ -0,0 +1,178 @@
+import { ChangeEvent, useEffect, useState } from "react";
+import { loadUsers } from "../../utils";
+import { VerificationSuccess } from "./VerificationSuccess";
+
+type VerificationFormProps = {
+  disabled: boolean;
+  host: string;
+};
+
+type UserDict = Record<string, string>;
+
+const submitForm = async (username: string, pubkey: string) => {
+  let options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, pubkey }),
+  };
+  let p = await fetch("/verify", options);
+  let response = await p.json();
+  return response;
+};
+
+export const VerificationForm = ({ disabled, host }: VerificationFormProps) => {
+  const [users, setUsers] = useState<boolean | UserDict>(false);
+  const [username, setUsername] = useState("");
+  const [pubkey, setPubkey] = useState("");
+
+  const [formError, setFormError] = useState<boolean | string>(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  useEffect(() => {
+    loadUsers(host).then(users => {
+      setUsers(users);
+    });
+  }, []);
+
+  const formDisabled = !users || disabled;
+  const hasUsername = username.length > 0;
+  const hasPubkey = pubkey.length > 0;
+
+  const showFormResult = !!formError || !!formSubmitted;
+
+  const checkUsername = (username: string) => {
+    if (username.length === 0) {
+      setFormError(false);
+      return;
+    }
+
+    const usernameFormat = /^[0-9a-z-_\.]{1,64}$/g;
+    const isUsernameValid = usernameFormat.test(username);
+
+    if (!isUsernameValid) {
+      setFormError(
+        "The username can only contain lowercase letters, numbers, _, - and ."
+      );
+    } else if ((users as UserDict)[username]) {
+      setFormError("Sorry, this username is already taken.");
+    } else {
+      setFormError(false);
+    }
+  };
+
+  const handleUsername = (event: ChangeEvent<HTMLInputElement>) => {
+    const newUsername = event.target.value.toLowerCase();
+
+    if (newUsername.length === 0) {
+      setFormError(false);
+      checkPubkey(pubkey);
+    } else {
+      checkUsername(newUsername);
+    }
+
+    setUsername(newUsername);
+  };
+
+  const checkPubkey = (pubkey: string) => {
+    if (pubkey.length === 0) {
+      setFormError(false);
+      return;
+    }
+
+    const pubkeyFormat = /^[0-9a-f]{1,64}$/g;
+    const isPubkeyValid = pubkeyFormat.test(pubkey);
+
+    if (!isPubkeyValid) {
+      setFormError("Please enter the HEX version of your public key.");
+    } else if (pubkey.length !== 64) {
+      setFormError("Your public key should be 64 characters long.");
+    } else if (Object.values(users).includes(pubkey)) {
+      setFormError("Sorry, this pubkey is already taken.");
+    } else {
+      setFormError(false);
+    }
+  };
+
+  const handlePubkey = (event: ChangeEvent<HTMLInputElement>) => {
+    const newPubkey = event.target.value.toLowerCase();
+
+    if (newPubkey.length === 0) {
+      setFormError(false);
+      checkUsername(username);
+    } else {
+      checkPubkey(newPubkey);
+    }
+
+    setPubkey(newPubkey);
+  };
+
+  const handleVerification = () => {
+    if (hasUsername && hasPubkey) {
+      // submitForm(username, pubkey).then(response => {
+      //   console.log("hit BE", response);
+      // });
+
+      const iftttEvent = "nostr_industries_verify";
+      const iftttUrl = `https://maker.ifttt.com/trigger/${iftttEvent}/with/key/dSscp_OKcfnlqc4jitN7yc`;
+      const dataUrl = iftttUrl + `?value1=${username}&value2=${pubkey}`;
+
+      fetch(dataUrl, {
+        mode: "no-cors",
+      });
+
+      setUsername("");
+      setPubkey("");
+
+      setFormSubmitted(true);
+    }
+  };
+
+  return (
+    <>
+      <form className="box verify-form">
+        <div>
+          <input
+            type="text"
+            placeholder="your-name"
+            maxLength={64}
+            disabled={formDisabled}
+            value={username}
+            onChange={handleUsername}
+          />
+          <label htmlFor="username">
+            <strong>@{host}</strong>
+          </label>
+        </div>
+
+        <div>
+          <input
+            type="text"
+            placeholder="your public key in HEX format*"
+            maxLength={64}
+            disabled={formDisabled}
+            value={pubkey}
+            onChange={handlePubkey}
+          />
+          <button
+            type="button"
+            disabled={formDisabled || !!formError || !hasUsername || !hasPubkey}
+            onClick={handleVerification}
+          >
+            verify
+          </button>
+        </div>
+      </form>
+
+      {showFormResult && (
+        <div className="box verify-result">
+          {formError && <div className="error">{formError}</div>}
+          {formSubmitted && (
+            <VerificationSuccess username={username} host={host} />
+          )}
+        </div>
+      )}
+    </>
+  );
+};
