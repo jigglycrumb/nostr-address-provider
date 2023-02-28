8e17e4cfb01b168d30de9df6cc4d822c9dc6fa6b
hpcodecraft <thesquidpeople@gmail.com>
2023-02-28 18:24:29 +0100

    feat: config file

---
+++ site.config.js
@@ -0,0 +1,14 @@
+export default {
+  // set this to your domain name
+  host: "nostr.industries",
+
+  // set this to true to disable verification site-wide
+  verificationDisabled: false,
+
+  // set this to your monthly costs
+  costsMonthly: "$4 USD",
+
+  // set this to your donation LNURL
+  donationLNUrl:
+    "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AKXUATJD3CZ7JMPVDV9YCC4FVREX",
+};
