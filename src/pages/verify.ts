612ad3581632b273c0937c929b6e0de743550c38
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2023-04-19 21:55:14 +0200

    revert last commit

--- src/pages/verify.ts
+++ src/pages/verify.ts
@@ -8,57 +8,57 @@ const dbName = "verification";
 const dbCollection = "names";
 
 export const post: APIRoute = async function post({ request }) {
-  // if (request.headers.get("Content-Type") === "application/json") {
-  let hasError = false;
-  const body = await request.json();
-  const { username, pubkey } = body;
+  if (request.headers.get("Content-Type") === "application/json") {
+    let hasError = false;
+    const body = await request.json();
+    const { username, pubkey } = body;
 
-  const usernameFormat = /^[0-9a-z-_\.]{1,64}$/g;
-  const isUsernameValid = usernameFormat.test(username);
+    const usernameFormat = /^[0-9a-z-_\.]{1,64}$/g;
+    const isUsernameValid = usernameFormat.test(username);
 
-  const pubkeyFormat = /^[0-9a-f]{1,64}$/g;
-  const isPubkeyValid = pubkeyFormat.test(pubkey);
+    const pubkeyFormat = /^[0-9a-f]{1,64}$/g;
+    const isPubkeyValid = pubkeyFormat.test(pubkey);
 
-  // stop here if username or pubkey are invalid
-  if (!isUsernameValid || !isPubkeyValid) {
-    hasError = true;
-  }
-
-  // Use connect method to connect to the server
-  await client.connect();
-  const db = client.db(dbName);
-  const collection = db.collection(dbCollection);
-
-  // find by username
-  const existingUsers = await collection.find({ username }).toArray();
-  // stop here if username exists
-  if (existingUsers.length > 0) {
-    hasError = true;
-  }
-
-  // find by pubkey
-  const existingPubkeys = await collection.find({ pubkey }).toArray();
-  // stop here if username exists
-  if (existingPubkeys.length > 0) {
-    hasError = true;
-  }
-
-  if (!hasError) {
-    const verifiedAt = new Date().toISOString();
-    await collection.insertOne({ username, pubkey, verifiedAt });
-  }
-
-  client.close();
-
-  return new Response(
-    JSON.stringify({
-      success: !hasError,
-    }),
-    {
-      status: hasError ? 400 : 200,
+    // stop here if username or pubkey are invalid
+    if (!isUsernameValid || !isPubkeyValid) {
+      hasError = true;
     }
-  );
-  // }
 
-  // return new Response(null, { status: 400 });
+    // Use connect method to connect to the server
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection(dbCollection);
+
+    // find by username
+    const existingUsers = await collection.find({ username }).toArray();
+    // stop here if username exists
+    if (existingUsers.length > 0) {
+      hasError = true;
+    }
+
+    // find by pubkey
+    const existingPubkeys = await collection.find({ pubkey }).toArray();
+    // stop here if username exists
+    if (existingPubkeys.length > 0) {
+      hasError = true;
+    }
+
+    if (!hasError) {
+      const verifiedAt = new Date().toISOString();
+      await collection.insertOne({ username, pubkey, verifiedAt });
+    }
+
+    client.close();
+
+    return new Response(
+      JSON.stringify({
+        success: !hasError,
+      }),
+      {
+        status: hasError ? 400 : 200,
+      }
+    );
+  }
+
+  return new Response(null, { status: 400 });
 };
